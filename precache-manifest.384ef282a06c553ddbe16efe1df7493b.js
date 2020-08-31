d926041869cb079e25571488b94e0f263ef0effc
System Administrator <root@Pranav-MacBook-Pro.local>
2020-08-30 20:51:58 -0700

    Updates

---
+++ precache-manifest.384ef282a06c553ddbe16efe1df7493b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3142bfd6524fed3cc4a70190941e3066",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7ac8acb40ffb8cb6026",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ccec3f271837289e6f65",
+    "url": "/static/css/main.71e2f2a3.chunk.css"
+  },
+  {
+    "revision": "c7ac8acb40ffb8cb6026",
+    "url": "/static/js/2.0789156f.chunk.js"
+  },
+  {
+    "revision": "b25e0c44d0b24745f7d62e2ea6be31e5",
+    "url": "/static/js/2.0789156f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccec3f271837289e6f65",
+    "url": "/static/js/main.e4680e9b.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
